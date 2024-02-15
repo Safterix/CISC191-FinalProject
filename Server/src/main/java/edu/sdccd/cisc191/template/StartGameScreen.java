f1b3592646df02a5eb3fd480974372d711f0777d
kimberlywong <kwong005@sdccd.edu>
2024-02-15 15:20:29 -0800

    made start screen class

---
+++ Server/src/main/java/edu/sdccd/cisc191/template/StartGameScreen.java
@@ -0,0 +1,21 @@
+package edu.sdccd.cisc191.template;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+public class StartGameScreen extends Application {
+    public static void main(String[]args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        //TODO make startscreen and many things
+    }
+}
